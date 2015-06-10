14b6967335c32641d3b6d74dfb0cdd645d92aa78
Ciprianna <ciprianna17@gmail.com>
2015-06-10 11:28:10 -0500

    Test is finished and runs successfully.

--- paragraph_truncator_test.rb
+++ paragraph_truncator_test.rb
@@ -1,3 +1,17 @@
 # Paragraph Truncator Test
 require "minitest/autorun"
 require_relative "paragraph_truncator.rb"
+
+class ParagraphTruncatorTest < Minitest::Test
+
+  # Need to test if the truncate_paragraph test returns a shortened character
+  # length with the correct cutoff characters than the original paragraph.
+  def test_truncate_paragraph
+    text_to_shorten = ParagraphTruncator.new(paragraph: "Lengthy text that goes on and on and on and on and on and on and on and on and on! Cats!")
+    output = text_to_shorten.truncate_paragraph(text_to_shorten.arguments)
+
+
+    assert(text_to_shorten.arguments[:character_length] + text_to_shorten.arguments[:cutoff_characters].length <= text_to_shorten.arguments[:paragraph].length)
+  end
+
+end
